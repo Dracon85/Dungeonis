b23e0877eb6ab81e8ac6803ac69db3c8f358e6e4
Matt500b <mattbarber1994@aol.com>
2017-04-22 21:09:24 +0100

    - Created a basic loading screen which comes up when loading a new scene and contains a progress bar. - Tidied up scripts

--- Dungeonis-Master/Assets/Characters/Player/Player.cs
+++ Dungeonis-Master/Assets/Characters/Player/Player.cs
@@ -4,85 +4,115 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 public class Player
-	: MonoBehaviour,IDamageable
+	: MonoBehaviour, IDamageable
 {
+	[SerializeField] private float _maxHealthPoints    = 100f;
+	[SerializeField] private float _playerAtkPower     = 10f;
+	[SerializeField] private float _minTimeBetweenHits = 0.5f;
+	[SerializeField] private float _maxAttackRange     = 2f;
+	[SerializeField] private int _enemyLayer           = 9;
+	[SerializeField] private GameObject _respawnPoint;
+	[SerializeField] private Weapon _weaponInUse;
 
-	[SerializeField]float maxHealthPoints    = 100f;
-	[SerializeField]float playerAtkPower     = 10f;
-	[SerializeField]float minTimeBetweenHits = 0.5f;
-	[SerializeField]float maxAttackRange     = 2f;
-	[SerializeField] int enemyLayer          = 9;
-	//set weapon in use and location for it to spawn
-	[SerializeField] Weapon weaponInUse;
-
-	GameObject respawnPoint;
-	float currentHealthPoints;
-	float lastHitTime = 0f;
-	CameraRaycaster cameraRaycaster;
-
-	void Start()
-	{
-		cameraRaycaster                            = FindObjectOfType<CameraRaycaster>();
-		cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
-		currentHealthPoints                        = maxHealthPoints;
-		PutWeaponInHand ();
-	}
-
-	private void PutWeaponInHand ()
-	{
-		var weaponPrefab = weaponInUse.GetWeaponPrefab();
-		GameObject dominantHand = RequestDominantHand ();
-		var weapon = Instantiate (weaponPrefab, dominantHand.transform);
-		weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
-		weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
-		//TODO move weapon to correct place and child to hand
-	}
-	//handle DominantHand attachment script
-	private GameObject RequestDominantHand (){
-		var dominantHands = GetComponentsInChildren<DominantHand>();
-		int numberOfDominantHands = dominantHands.Length;
-		Assert.IsFalse(numberOfDominantHands<=0,"No Dominant Hand Found, on Player Please Add one to Bone"); 
-		Assert.IsFalse(numberOfDominantHands>1,"Multiple Dominant Hand scripts on Player Please use only one");
-		return dominantHands[0].gameObject; 
-	}
-
-	//mouse click attack if within range...TODO consider using weapon collider to do damage to enemies
-	void OnMouseClick (RaycastHit raycastHit, int layerHit)
-	{
-		if (layerHit == enemyLayer)
-		{
-			GameObject enemy = raycastHit.collider.gameObject;
-
-			if((enemy.transform.position-transform.position).magnitude > maxAttackRange)
-				return;
-
-			var enemyComponent = enemy.GetComponent<Enemy> ();
-
-			if (Time.time-lastHitTime>minTimeBetweenHits)
-			{
-				enemyComponent.TakeDamage(playerAtkPower);
-				lastHitTime = Time.time;
-			}
-		}
-	}
+	private float _currentHealthPoints;
+	private float _lastHitTime = 0f;
+	private CameraRaycaster _cameraRaycaster;
 
 	public float healthAsPercentage
 	{
 		get
 		{
-			return currentHealthPoints/maxHealthPoints;
+			return _currentHealthPoints / _maxHealthPoints;
 		}
 	}
 
+	/// <summary>
+	/// Applies damage taken to player.
+	/// </summary>
+	/// <param name="damage">The damage.</param>
 	public void TakeDamage(float damage)
 	{
-		currentHealthPoints     = Mathf.Clamp(currentHealthPoints-damage, 0f, maxHealthPoints);
-		respawnPoint            = GameObject.FindGameObjectWithTag("PlayerRespawn");
+		_currentHealthPoints = Mathf.Clamp(_currentHealthPoints - damage, 0f, _maxHealthPoints);
 
-		if (currentHealthPoints <= 0)
+		if (IsPlayerDead())
 		{
-			transform.position  = respawnPoint.transform.position;
-			currentHealthPoints = maxHealthPoints;
+			transform.position   = _respawnPoint.transform.position;
+			_currentHealthPoints = _maxHealthPoints;
+		}
+	}
+
+	private void Awake()
+	{
+		_respawnPoint      = GameObject.FindGameObjectWithTag("PlayerRespawn");
+		transform.position = _respawnPoint.transform.position;
+	}
+
+	private void Start()
+	{
+		_cameraRaycaster                            = FindObjectOfType<CameraRaycaster>();
+		_cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
+		_currentHealthPoints                        = _maxHealthPoints;
+		PutWeaponInHand();
+	}
+
+	/// <summary>
+	/// Determines whether the player is dead.
+	/// </summary>
+	/// <returns>
+	/// <c>true</c> if player is dead; otherwise, <c>false</c>.
+	/// </returns>
+	private bool IsPlayerDead()
+	{
+		return _currentHealthPoints <= 0;
+	}
+
+	/// <summary>
+	/// Puts the weapon in hand.
+	/// </summary>
+	private void PutWeaponInHand()
+	{
+		var weaponPrefab               = _weaponInUse.GetWeaponPrefab();
+		GameObject dominantHand        = RequestDominantHand();
+		var weapon                     = Instantiate(weaponPrefab, dominantHand.transform);
+		weapon.transform.localPosition = _weaponInUse.gripTransform.localPosition;
+		weapon.transform.localRotation = _weaponInUse.gripTransform.localRotation;
+		//TODO move weapon to correct place and child to hand
+	}
+
+	/// <summary>
+	/// Requests the dominant hand. Handle DominantHand attachment script
+	/// </summary>
+	/// <returns>The Dominant Hand gameObject</returns>
+	private GameObject RequestDominantHand()
+	{
+		var dominantHands                     = GetComponentsInChildren<DominantHand>();
+		int numberOfDominantHands             = dominantHands.Length;
+		Assert.IsFalse(numberOfDominantHands <= 0, "No Dominant Hand Found, on Player Please Add one to Bone");
+		Assert.IsFalse(numberOfDominantHands > 1, "Multiple Dominant Hand scripts on Player Please use only one");
+		return dominantHands[0].gameObject;
+	}
+
+	/// <summary>
+	/// Called when [mouse click]. Attacks enemies if within range. TODO consider using weapon collider to do damage to enemies
+	/// </summary>
+	/// <param name="raycastHit">The raycast hit.</param>
+	/// <param name="layerHit">The layer hit.</param>
+	private void OnMouseClick (RaycastHit raycastHit, int layerHit)
+	{
+		if (layerHit.Equals(_enemyLayer))
+		{
+			GameObject enemy = raycastHit.collider.gameObject;
+
+			if((enemy.transform.position-transform.position).magnitude > _maxAttackRange)
+				return;
+
+			var enemyComponent = enemy.GetComponent<Enemy>();
+
+			if ((Time.time - _lastHitTime) > _minTimeBetweenHits)
+			{
+				enemyComponent.TakeDamage(_playerAtkPower);
+				_lastHitTime = Time.time;
+			}
 		}
 	}
 }
--- Dungeonis-Master/Assets/Characters/Player/PlayerMovement.cs
+++ Dungeonis-Master/Assets/Characters/Player/PlayerMovement.cs
@@ -1,34 +1,27 @@
-﻿using System;
-using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+﻿using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 [RequireComponent(typeof(ThirdPersonCharacter))]
-public class PlayerMovement2: MonoBehaviour
+public class PlayerMovement
+	: MonoBehaviour
 {
+	public float speed = 10;
+
 	private ThirdPersonCharacter _character;
 	private Transform _cam;
-	private Vector3 _camForward;
 	private Vector3 _move;
 	private bool _jump;
-	private Vector3 _movement;
-	public float speed = 10;
 
 	private void Start()
 	{
-		// get the transform of the main camera
 		if (Camera.main != null)
 			_cam = Camera.main.transform;
 		else
-		{
 			Debug.LogWarning("Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-			// we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-		}
 
-		// get the third person character ( this should never be null due to require component )
-		_character       = GetComponent<ThirdPersonCharacter>();
+		// Get the third person character ( this should never be null due to require component )
+		_character = GetComponent<ThirdPersonCharacter>();
 	}
-
 
 	private void Update()
 	{
@@ -39,22 +32,18 @@ public class PlayerMovement2: MonoBehaviour
 	// Fixed update is called in sync with physics
 	private void FixedUpdate()
 	{
-		// read inputs
-
 		float horizontalAxis = Input.GetAxis("Horizontal");
 		float verticalAxis   = Input.GetAxis("Vertical");
 		bool crouch          = Input.GetKey(KeyCode.C);     //TODO: Map this in menus
 
-		// calculate move direction to pass to character
+		// Calculate move direction to pass to character
 		if (_cam != null)
 		{
-			_move = verticalAxis* _cam.forward.normalized + horizontalAxis* _cam.right.normalized;
+			_move = verticalAxis * _cam.forward.normalized + horizontalAxis * _cam.right.normalized;
 
-			// walk speed multiplier
 			if (Input.GetKey(KeyCode.LeftShift))
 				_move *= 0.5f;
 
-			// pass all parameters to the character control script
 			_character.Move(_move, crouch, _jump);
 			_jump = false;
 		}
